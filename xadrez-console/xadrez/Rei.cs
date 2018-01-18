aa08618a9c95c9d613252a73df02527c593b8225
RobertoSalcedoAlves <roberto.salcedoalves@gmail.com>
2018-01-18 17:56:00 -0200

    commit intermediario validando o roque

--- xadrez-console/xadrez/Rei.cs
+++ xadrez-console/xadrez/Rei.cs
@@ -15,10 +15,74 @@ namespace xadrez{
             return p == null || p.cor != this.cor;
         }
 
+        //poupa linhas invertendo o sinal
+        private int inverteLado(){    
+            if(this.cor == Cor.Branca){
+                return 1;
+            }else{
+               return -1;
+            }
+        }
+
+        private int saltoRoque(string TipoRoque_grande_pequeno){
+            int salto = 0;
+            if(TipoRoque_grande_pequeno == "grande"){
+                salto = 2;
+            }else if(TipoRoque_grande_pequeno == "pequeno"){
+                salto = 1;
+            }else{
+                throw new TabuleiroException("Erro interno: erro na definição: TipoRoque_grande_pequeno");
+            }
+            return salto;
+        }
+
+         private bool temVizinho(Posicao posicao, string TipoRoque_p_g){
+            Posicao pos = new Posicao(0, 0);
+            int salto = 0;
+            if(TipoRoque_p_g == "p"){
+                salto = saltoRoque("pequeno");
+                for(var i = 0; i < salto; i++){
+                    pos.definirValores(posicao.linha, posicao.coluna + salto + i * inverteLado());
+                    if(tab.existePeca(pos)){
+                        return true;
+                    }
+                }
+            }else if (TipoRoque_p_g == "g"){
+                salto = saltoRoque("grande");
+                for(var i = 0; i < salto; i++){
+                    pos.definirValores(posicao.linha, posicao.coluna + salto + i * inverteLado());
+                    if(tab.existePeca(pos)){
+                        return true;
+                    }
+                }
+            }else{
+                throw new TabuleiroException("Erro interno: erro na definição: 'TipoRoque_p_g'");
+            }
+            return false;
+        }
+
         public override bool[,] movimentosPossiveis() {
             bool[,] mat = new bool[tab.linhas, tab.colunas];
 
             Posicao pos = new Posicao(0, 0);
+           
+
+            //RoquePequeno
+            if(!temVizinho(posicao, "p") && qteMovimentos == 0 && tab.peca){
+                pos.definirValores(posicao.linha, posicao.coluna + 2 * inverteLado());
+                if(tab.posicaoValida(pos) && podeMover(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+
+            //RoqueGrande
+            if(!temVizinho(posicao, "p")){
+                pos.definirValores(posicao.linha, posicao.coluna + 2 * inverteLado());
+                if(tab.posicaoValida(pos) && podeMover(pos)) {
+                    mat[pos.linha, pos.coluna] = true;
+                }
+            }
+
 
             //acima
             pos.definirValores(posicao.linha -1, posicao.coluna);
